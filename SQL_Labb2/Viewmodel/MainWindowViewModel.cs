c500857202b37ab26dca08117c1dc702e586f997
Slides12 <djohan93@hotmail.com>
2024-11-28 17:35:07 +0100

    Started SQL_Labb2

---
+++ SQL_Labb2/Viewmodel/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SQL_Labb2.Command;
+
+namespace SQL_Labb2.Viewmodel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+
+        public DelegateCommand FullscreenCommand { get; }
+        public DelegateCommand ExitCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            FullscreenCommand = new DelegateCommand(SetFullscreen);
+            ExitCommand = new DelegateCommand(Exit);
+        }
+
+
+
+        private void SetFullscreen(object obj)
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+
+            }
+        }
+
+        private void Exit(object obj)
+        {
+
+            Application.Current.Shutdown();
+        }
+    }
+}
+
+
---
+++ SQL_Labb2/Viewmodel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Labb2.Viewmodel
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaiseProperyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
